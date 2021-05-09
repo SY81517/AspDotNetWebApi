d3ff915ae41c3690eea0df81297aa62c44755d7a
SY81517 <63453966+SY81517@users.noreply.github.com>
2021-05-09 20:26:31 +0900

    モデルバインドのためし、コミット

---
+++ MessageBindeTest/Controllers/Test2Controller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MessageBindeTest.Controllers
+{
+    [RoutePrefix("api/test2")]
+    public class Test2Controller : ApiController
+    {
+        [HttpPost]
+        [Route("fire")]
+        public void Fire( MyData o )
+        {
+            Console.WriteLine();
+        }
+    }
+
+    public class MyData
+    {
+        public string ID { get; set; }
+        public  MyObject ObMyObject { get; set; }
+    }
+}
---
+++ MessageBindeTest/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MessageBindeTest.Controllers
+{
+    [RoutePrefix("api/test")]
+    public class TestController : ApiController
+    {
+        [HttpPost]
+        [Route("takeit")]
+        public void TakeIT(MyObject o)
+        {
+            Console.WriteLine();
+        }
+    }
+
+    public class MyObject
+    {
+        public string V1 { get; set; }
+        public string V2 { get; set; }
+    }
+}
