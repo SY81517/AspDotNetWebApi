815188634d490b7c6f0de530a0af27c2c7e005d5
SY81517 <63453966+SY81517@users.noreply.github.com>
2021-05-19 04:15:17 +0900

    実行結果のバックアップ

--- ConsoleHttpClientApp/Program.cs
+++ ConsoleHttpClientApp/Program.cs
@@ -1,100 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Configuration;
-using System.Diagnostics;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
-
 
 namespace ConsoleHttpClientApp
 {
     class Program
     {
-        /// <summary>
-        /// HTTPクライアント
-        /// </summary>
-        /// <remarks>
-        /// 1度だけインスタンス化し、アプリケーション全体で使い回す
-        /// </remarks>
-        private static readonly HttpClient Client = new HttpClient();
+        private static readonly ValuesApi _valuesApi = new ValuesApi();
 
         static void Main( string[] args )
         {
-            RunAsync().GetAwaiter().GetResult();
+            Console.WriteLine("Start ");
+            _valuesApi.GetAsync().GetAwaiter().GetResult();
+            _valuesApi.PutAsync().GetAwaiter().GetResult();
+            Console.WriteLine("End");
         }
 
-        /// <summary>
-        /// 初期化
-        /// </summary>
-        static void Setting()
-        {
-            Client.BaseAddress = new Uri("http://localhost:54443/api/values");
-            Client.DefaultRequestHeaders.Accept.Clear();
-            Client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/json"));
-        }
-
-        static async Task RunAsync()
-        {
-            Setting();
-            var stopwatch = new Stopwatch();
-            var recordTimeSpans = new List<TimeSpan>();
-            for (var i = 0; i < 10; i++)
-            {
-                var result = AppResult.Ok;
-                stopwatch.Reset();
-                stopwatch.Start();
-                try
-                {
-                    // GET api/values
-                    var resource = await GetAllAsync(Client.BaseAddress);
-                }
-                catch(Exception e)
-                {
-                    Console.WriteLine(e.ToString());
-                    result = AppResult.Ng;
-                }
-                finally
-                {
-                    stopwatch.Stop();
-                    recordTimeSpans.Add(stopwatch.Elapsed);
-                    var resultStr = $"i={i},Time={stopwatch.Elapsed.Seconds:00}.{stopwatch.Elapsed.Milliseconds:000}[sec],Result={result}";
-                    Console.WriteLine(resultStr);
-                }
-            }
-
-            var doubleAverageTicks = recordTimeSpans.Average(timeSpan => timeSpan.Ticks);
-            var longAverageTicks = Convert.ToInt64(doubleAverageTicks);
-            var average = new TimeSpan(longAverageTicks);
-            Console.WriteLine($"Average={average.Seconds:00}.{average.Milliseconds:000}[sec]");
-        }
-
-        /// <summary>
-        /// GET APIをコール
-        /// </summary>
-        /// <param name="path">RI</param>
-        /// <returns></returns>
-        static async Task<IEnumerable<string>> GetAllAsync(Uri uri)
-        {
-            IEnumerable<string> resource = null;
-            // Get要求
-            var response = await Client.GetAsync(uri);
-            //if (response.IsSuccessStatusCode)
-            //{
-            //    //リソース取得
-            //    resource = await response.Content.ReadAsAsync<IEnumerable<string>>();
-            //}
-            return resource;
-        }
     }
 
-    public enum AppResult
-    {
-        Ok,
-        Ng
-    }
 }
---
+++ ConsoleHttpClientApp/ValuesApi.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsoleHttpClientApp
+{
+    public class ValuesApi
+    {
+        /// <summary>
+        /// HTTPクライアント
+        /// </summary>
+        /// <remarks>
+        /// 1度だけインスタンス化し、アプリケーション全体で使い回す
+        /// </remarks>
+        private static readonly HttpClient Client = new HttpClient();
+
+        private static readonly string ApiUri = "http://localhost:54443/api/values";
+
+        public async Task GetAsync()
+        {
+            try
+            {
+                // GET api/values
+                var resource = await Client.GetAsync(ApiUri);
+                resource.EnsureSuccessStatusCode();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("GET SUCCESS");
+        }
+
+        public async Task PutAsync()
+        {
+            try
+            {
+                var testInput = "TestMessage";
+                var jsonString = JsonConvert.SerializeObject(testInput);
+                var data = new StringContent(jsonString, Encoding.UTF8, mediaType: "application/json");
+                var response = await Client.PutAsync("http://localhost:54443/api/values/5", data);
+                response.EnsureSuccessStatusCode();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("PUT SUCCESS");
+        }
+    }
+}
